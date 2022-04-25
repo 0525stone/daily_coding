30cd911c60d9cace0876ce240a8c2379687c8a24
yunsuk lee <0525stone@gmail.com>
2022-04-25 16:32:29 +0900

    dev function done

--- programmers/42586_dev_function.py
+++ programmers/42586_dev_function.py
@@ -14,8 +14,6 @@ link : https://programmers.co.kr/learn/courses/30/lessons/42586
 작업 속도는 100 이하의 자연수입니다.
 배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
 
-
-
 """
 def solution(progresses, speeds):
     """
@@ -27,9 +25,7 @@ def solution(progresses, speeds):
     """
     answer = []
     
-    # for progress, speed in zip(progresses, speeds):
     while 1:
-
         temp = 0
         progress = progresses[0]
         speed = speeds[0]
@@ -51,11 +47,9 @@ def solution(progresses, speeds):
                 temp+=1
             else:
                 break
+        answer.append(temp)
         
-        if temp:
-            answer.append(temp)
-        
-        # progress가 없으면 종료
+        # 배포할 progress가 없으면 종료
         if not progresses:
             break
 
