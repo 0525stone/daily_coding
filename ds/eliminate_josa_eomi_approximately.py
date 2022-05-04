d2a2ddd4c847e006e471b4e9746810ad0ba32cd2
yunsuk lee <0525stone@gmail.com>
2022-05-04 13:33:48 +0900

    rename function name

--- ds/eliminate_josa_eomi_approximately.py
+++ ds/eliminate_josa_eomi_approximately.py
@@ -21,12 +21,12 @@ def read_josa_eomi(filename='./data/JosaEomi/EOMI.TXT'):
         dump = dumps.split('\n')
         
         # if에 사용할 eomi 조건문으로 사용하기 위한 문자열
-        eomi_if = ' | '.join(dump)
+        eomi_if = '|'.join(dump[:-1])
     
     return dump[:-1]  # 마지막에 공백 문자가 있어서 그것은 제외시켜줘야함
 
 
-def eliminate_josa_eomi_approximately_regex(text, sub1='', sub2=''):
+def eliminate_josa_eomi_approximately_regex_loop(text, sub1='', sub2=''):
     """
     인터페이스 
     - 인자로 어미,조사 입력으로 줘야함
@@ -35,12 +35,7 @@ def eliminate_josa_eomi_approximately_regex(text, sub1='', sub2=''):
     """
     result = ''
 
-    # # read josa, eomi files
-    # eomi_filename = './data/JosaEomi/EOMI.TXT'
-    # josa_filename = './data/JosaEomi/JOSA.TXT'
-    # eomi = read_josa_eomi(eomi_filename)
-    # josa = read_josa_eomi(josa_filename)
-
+    # for문으로 일일이 조사어미와 비교하여 제거
     # 입력 text와 eomi, josa를 비교하여 없는 것들 제거
     for sub in sub1:
         """
@@ -56,15 +51,15 @@ def eliminate_josa_eomi_approximately_regex(text, sub1='', sub2=''):
         따라서, or구문으로 전체를 잡아서 해줘야 깔끔하게 사라질 것으로 예상..
         또는 불용어 부분을 /와 같은 특수문자로 바꿔놓고 마지막에 특수문자를 제거하는 식으로 해도 될 듯
         """
-        if sub in text:
+        text = re.sub(r"([?.,!¿])", " ", text) 
+        # text = re.sub(r'\n', ' ',text)
+
+        if f'{sub} ' in text:
             print(f'it has {sub}\nbefore {text}')
             # text = re.sub(r'{em}', '', text)   # em 다음에 스페이스(공백) 있는 것을 포함시켜야함(공백문자 표현식 있을 것임)
             # text = re.sub(r'[^가-힣]', '', text)
-            text = re.sub(f'{sub}', '', text)
+            text = re.sub(f'{sub} ', ' ', text)
             print(f'after {text}')
-
-
-    # result = '현수 술주정뱅이'
 
     return text
 
@@ -85,19 +80,14 @@ def test_josa_eomi():
     2. eomis, josas 전부로 test 해볼 것
     3. context(문장들) 단위로 test 해볼 것
     """
-    texts = ['현수는 술주정뱅이다', '알코올 의존증을 치료할 생각이 있을쏘냐?', '자전거 여행을 가고 싶다.',  '거짓말로 들통 났다.']
-    answers = ['현수 술주정뱅이'  , '알코올 의존증 치료 필요', '자전거 여행 가 싶', '거짓말 들통 났']
+    texts = ['알코올 의존증을 치료할 생각이 있을쏘냐?', '자전거 여행을 가고 싶다.',  '거짓말로 들통 났다.', '현수는 술주정뱅이다.',]
+    answers = ['알코올 의존증 치료할 생각 있을쏘냐 ', '자전거 여행 가 싶 ', '거짓말 들통 났 ', '현수 술주정뱅 ' ,]  # 술주정뱅이=>술주정뱅
     
     for text, answer in zip(texts, answers):
-        assert eliminate_josa_eomi_approximately_regex(text, josas)==answer
+        assert eliminate_josa_eomi_approximately_regex_loop(text, josas)==answer
     
-    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='현수 술주정뱅이'
-    # text = '알코올 의존증을 치료하려면 무엇이 필요할까?'
-    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='알코올 의존증 치료 필요'
-    # text = '자전거 여행을 가고 싶다.'
-    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='자전거 여행 가 싶'
-    # text = '거짓말로 들통 났다.'
-    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='거짓말 들통 났'
+    print('current test cases done')
+
 
 if __name__=="__main__":
     test_josa_eomi()
