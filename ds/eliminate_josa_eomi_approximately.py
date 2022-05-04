d38736fc1a5120b0e367b70dc6fa6644e8cd5893
yunsuk lee <0525stone@gmail.com>
2022-05-04 11:15:22 +0900

    start on new day

--- ds/eliminate_josa_eomi_approximately.py
+++ ds/eliminate_josa_eomi_approximately.py
@@ -13,17 +13,36 @@ def eliminate_josa_eomi_approximately_mecab():
     pass
 
 
-def eliminate_josa_eomi_approximately_regex(text):
+# TODO : 이름이 이게 맞는지.. 이 프로그램 안에서는 조사,어미만 읽어오는 것이니까 이렇게 하는게 맞을까?
+def read_josa_eomi(filename='./data/JosaEomi/EOMI.TXT'):
+    # JosaEomi/ EOMI.TXT  JOSA.TXT
+    with open(filename,'r', encoding='euc-kr') as f:
+        dumps = f.read()
+        dump = dumps.split('\n')
+        
+        # if에 사용할 eomi 조건문으로 사용하기 위한 문자열
+        eomi_if = ' | '.join(dump)
+    
+    return dump[:-1]  # 마지막에 공백 문자가 있어서 그것은 제외시켜줘야함
+
+
+def eliminate_josa_eomi_approximately_regex(text, sub1='', sub2=''):
+    """
+    인터페이스 
+    - 인자로 어미,조사 입력으로 줘야함
+    - 함수자체로 동작하게끔
+    
+    """
     result = ''
 
-    # read josa, eomi files
-    eomi_filename = './data/JosaEomi/EOMI.TXT'
-    josa_filename = './data/JosaEomi/JOSA.TXT'
-    eomi = read_josa_eomi(eomi_filename)
-    josa = read_josa_eomi(josa_filename)
+    # # read josa, eomi files
+    # eomi_filename = './data/JosaEomi/EOMI.TXT'
+    # josa_filename = './data/JosaEomi/JOSA.TXT'
+    # eomi = read_josa_eomi(eomi_filename)
+    # josa = read_josa_eomi(josa_filename)
 
     # 입력 text와 eomi, josa를 비교하여 없는 것들 제거
-    for em in eomi:
+    for sub in sub1:
         """
         for문으로 불용어를 제거할 경우, 하나씩 제거하는 것이 되어서, 단어가 점점 사라질 수 있지 않나???
         ex
@@ -37,32 +56,19 @@ def eliminate_josa_eomi_approximately_regex(text):
         따라서, or구문으로 전체를 잡아서 해줘야 깔끔하게 사라질 것으로 예상..
         또는 불용어 부분을 /와 같은 특수문자로 바꿔놓고 마지막에 특수문자를 제거하는 식으로 해도 될 듯
         """
-        if em in text:
-            print(f'it has {em}\nbefore {text}')
+        if sub in text:
+            print(f'it has {sub}\nbefore {text}')
             # text = re.sub(r'{em}', '', text)   # em 다음에 스페이스(공백) 있는 것을 포함시켜야함(공백문자 표현식 있을 것임)
             # text = re.sub(r'[^가-힣]', '', text)
-            text = re.sub(em, '', text)
+            text = re.sub(f'{sub}', '', text)
             print(f'after {text}')
 
 
-    result = '현수 술주정뱅이'
+    # result = '현수 술주정뱅이'
 
-    return result
+    return text
 
 
-# TODO : 이름이 이게 맞는지.. 이 프로그램 안에서는 조사,어미만 읽어오는 것이니까 이렇게 하는게 맞을까?
-def read_josa_eomi(filename='./data/JosaEomi/EOMI.TXT'):
-    # JosaEomi/ EOMI.TXT  JOSA.TXT
-    with open(filename,'r', encoding='euc-kr') as f:
-        dumps = f.read()
-        dump = dumps.split('\n')
-        
-        # if에 사용할 eomi 조건문으로 사용하기 위한 문자열
-        eomi_if = ' | '.join(dump)
-        # print(f'" {eomi_if} "')
-    
-    return dump[:-1]  # 마지막에 공백 문자가 있어서 그것은 제외시켜줘야함
-        
 
 def test_josa_eomi():
     # test : read_josa_eomi()  => 직접 일일이 셀 수 없으니까, txt 파일의 line 수로 확인
@@ -73,20 +79,25 @@ def test_josa_eomi():
     print('read_josa_eomi test done')
     
 
-
-
-    eliminate_josa_eomi_approximately_regex('이거하려면 이것들이 필요하련만 과연?')
-
-
     # test final result
-    text = '현수는 술주정뱅이이다.'
-    assert eliminate_josa_eomi_approximately_regex(text)=='현수 술주정뱅이'
-    text = '알코올 의존증을 치료하려면 무엇이 필요할까?'
-    assert eliminate_josa_eomi_approximately_regex(text)=='알코올 의존증 치료 필요'
-    text = '자전거 여행을 가고 싶다.'
-    assert eliminate_josa_eomi_approximately_regex(text)=='자전거 여행 가 싶'
-    text = '거짓말로 들통 났다.'
-    assert eliminate_josa_eomi_approximately_regex(text)=='거짓말 들통 났'
+    """
+    1. test를 할 때, 처음에는 josas 만으로 잘 제거하는지 확인\
+    2. eomis, josas 전부로 test 해볼 것
+    3. context(문장들) 단위로 test 해볼 것
+    """
+    texts = ['현수는 술주정뱅이다', '알코올 의존증을 치료할 생각이 있을쏘냐?', '자전거 여행을 가고 싶다.',  '거짓말로 들통 났다.']
+    answers = ['현수 술주정뱅이'  , '알코올 의존증 치료 필요', '자전거 여행 가 싶', '거짓말 들통 났']
+    
+    for text, answer in zip(texts, answers):
+        assert eliminate_josa_eomi_approximately_regex(text, josas)==answer
+    
+    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='현수 술주정뱅이'
+    # text = '알코올 의존증을 치료하려면 무엇이 필요할까?'
+    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='알코올 의존증 치료 필요'
+    # text = '자전거 여행을 가고 싶다.'
+    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='자전거 여행 가 싶'
+    # text = '거짓말로 들통 났다.'
+    # assert eliminate_josa_eomi_approximately_regex(text,eomis)=='거짓말 들통 났'
 
 if __name__=="__main__":
     test_josa_eomi()
