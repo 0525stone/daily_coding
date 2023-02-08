56834c6b143633c3bfc60733690a5618057f157c
johnlee <0525stone@gmail.com>
2023-02-08 16:28:26 +0900

    .

--- programmers/42583_bridge_truck.py
+++ programmers/42583_bridge_truck.py
@@ -24,10 +24,6 @@ def solution(bridge_length, weight, truck_weights):
 
 if __name__=="__main__":
 
-    bridge_length, weight, truck_weights = 2, 10, [7,4,5,6]
-    answer = solution(bridge_length, weight, truck_weights)
-    assert answer==8, answer
-
     bridge_length, weight, truck_weights = 100, 100, [10]
     answer = solution(bridge_length, weight, truck_weights)
     assert answer==101, answer
@@ -35,3 +31,10 @@ if __name__=="__main__":
     bridge_length, weight, truck_weights = 100, 100, [10,10,10,10,10,10,10,10,10,10]
     answer = solution(bridge_length, weight, truck_weights)
     assert answer==110, answer
+    print("done")
+
+    bridge_length, weight, truck_weights = 2, 10, [7,4,5,6]
+    answer = solution(bridge_length, weight, truck_weights)
+    assert answer==8, answer
+
+    print("all done")
