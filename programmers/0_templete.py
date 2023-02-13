a3c9ab3e06b8e989958a66ec0305e140c2adbbdf
johnlee <0525stone@gmail.com>
2023-02-13 12:20:12 +0900

    프로그래머스 템플릿 만들어놓음

---
+++ programmers/0_templete.py
@@ -0,0 +1,18 @@
+"""
+templete for programmers
+"""
+
+# 문제마다 주어지는 형태 복사 붙여넣기하기
+def solution():
+    return 0
+
+
+
+if __name__=="__main__":
+    
+    # test case 추가해서 답 확인해주기
+    answer=solution()
+    assert answer==0, answer
+
+    print("all done")
+
