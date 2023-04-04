76c15049e677a1d540274ca7cd5a92f06e3f8607
0525stone <0525stone@gmail.com>
2023-04-04 12:04:06 +0900

    TODO : templete 작업

---
+++ twim/opencv_templete.py
@@ -0,0 +1,19 @@
+"""
+내가 쓸 때 복붙해서 사용할 opencv 관련 templete    230331
+requirements.txt 도 만들어야함
+"""
+
+
+class Vision_lys():
+    def __init__(self):
+        pass
+    def run(self, heightData):
+        pass
+
+
+def main():
+    print("hello")
+
+
+if __name__=="__main__":
+    main()
