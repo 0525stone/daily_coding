f9c9002ace6e045abc6886593708ffc8cebe74ef
Yunsuk Lee <0525stone@gmail.com>
2023-04-17 13:55:09 +0900

    before applying weldingtest codes

--- twim/example.py
+++ twim/example.py
@@ -1,53 +1,39 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
-from PySide6.QtGui import QPainter, QPen, QColor
-from PySide6.QtCore import Qt, QPoint
+import sys
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt
 
-class Canvas(QWidget):
+from random import randint, choice 
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Set the mouse tracking flag to track mouse movements
-        self.setMouseTracking(True)
+        self.label = QtWidgets.QLabel()
+        canvas = QtGui.QPixmap(400, 300)
+        canvas.fill(Qt.white)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+        self.draw_something()
 
-        # Initialize the list of lines to be drawn
-        self.lines = []
+    def draw_something(self):
+        """
+        
+        """
+        canvas = self.label.pixmap()
+        painter = QtGui.QPainter(canvas)
+        pen = QtGui.QPen()
+        pen.setWidth(15)
+        pen.setColor(QtGui.QColor('blue'))
+        painter.setPen(pen)
+        painter.drawLine(
+            QtCore.QPoint(100, 100),
+            QtCore.QPoint(300, 200)
+        )
+        painter.end()
+        self.label.setPixmap(canvas)
 
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
-        for line in self.lines:
-            painter.drawLine(line[0], line[1])
 
-    def mousePressEvent(self, event):
-        self.last_point = event.pos()
-
-    def mouseMoveEvent(self, event):
-        new_point = event.pos()
-        self.lines.append((self.last_point, new_point))
-        self.last_point = new_point
-        self.update()
-
-    def mouseReleaseEvent(self, event):
-        new_point = event.pos()
-        self.lines.append((self.last_point, new_point))
-        self.last_point = None
-        self.update()
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        # Create a canvas for drawing
-        self.canvas = Canvas()
-        self.setCentralWidget(self.canvas)
-
-if __name__ == '__main__':
-    # Create a PySide6 application instance
-    app = QApplication()
-
-    # Create a main window instance
-    main_window = MainWindow()
-    main_window.show()
-
-    # Run the application
-    app.exec()
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
