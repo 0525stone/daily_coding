c0a76104b99a8f9f4d096622afcd51e171ce3ad9
Yunsuk Lee <0525stone@gmail.com>
2023-04-27 09:30:31 +0900

    .

--- lys_modules/vp_metric_txt.py
+++ lys_modules/vp_metric_txt.py
@@ -166,17 +166,18 @@ if __name__=="__main__":
                     "/Users/johnlee/git/daily_coding/vp_data/result_ava_val_f_vy_false"
                     ]
     elif dataset_name=="flickr":
-        gt_path = "/Users/johnlee/git/daily_coding/vp_data/gt_flickr" # "D:\git\data_txt/vp-labels/gt_ava"
+        dir_root = "D:\git\data_txt"
+        gt_path = f"{dir_root}/vp-labels/gt_ava"# "/Users/johnlee/git/daily_coding/vp_data/gt_flickr" # "D:\git\data_txt/vp-labels/gt_ava"
         result_paths = [
-                "/Users/johnlee/git/daily_coding/vp_data/result_flickr", 
+                "/Users/johnlee/git/daily_coding/vp_data/result_flickr", #"/Users/johnlee/git/daily_coding/vp_data/result_flickr", 
                 "/Users/johnlee/git/daily_coding/vp_data/result_flickr_geo_false_vy_false",
                 "/Users/johnlee/git/daily_coding/vp_data/result_filckr_vy_false",
                     "/Users/johnlee/git/daily_coding/vp_data/result_flickr_val_f_vy_false"
         ]
-
-    file_txt = "/Users/johnlee/git/daily_coding/vp_data/valid.txt" # D:/git/DeepGuider/bin/data/tmm17/valid.txt
-    gt_path = "/Users/johnlee/git/daily_coding/vp_data/gt_valid"
-    result_paths = "/Users/johnlee/git/daily_coding/vp_data/result_valid3"
+    dir_root = "D:/git/data_txt"
+    file_txt = f"{dir_root}/valid.txt" # D:/git/DeepGuider/bin/data/tmm17/valid.txt
+    gt_path = f"{dir_root}/gt_valid"
+    result_paths = f"{dir_root}/result_valid1"
     
     VP = vp_metric_txt(file_txt, gt_path, result_paths)
     VP.result_summary()
