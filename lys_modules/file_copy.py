504b4d3e46f322865ee8ab65b63dc0dd776773a1
0525stone <0525stone@gmail.com>
2023-04-30 23:04:29 +0900

    su3 sampling and copying code

--- lys_modules/file_copy.py
+++ lys_modules/file_copy.py
@@ -1,4 +1,5 @@
 import os
+import random
 import shutil
 from tqdm import tqdm
 
@@ -23,21 +24,24 @@ root - 000 - file.file
 #         shutil.copy(from_filename, to_filename)
 # #         pass
 
-# # for su3 case
-# dir_root = "C:/git/DeepGuider/bin/data/su3"
-# dir_list = os.listdir(dir_root)
-# dst_root = 'C:/git/DeepGuider/bin/data/su3_images'
-# for d in dir_list: # TODO : tqdm
-#     file_root = f"{dir_root}/{d}"
-#     file_list = os.listdir(file_root)
-#     for f in file_list:
-#         if '.png' in f:
-#             filename = f"{f}.jpg"
-            
-#             from_filename = f"{file_root}/{f}"
-#             to_filename = f"{dst_root}/{d}_{f}"
-# #             print(f"{from_filename}\n{to_filename}\n\n")
-#             shutil.copy(from_filename, to_filename)
+def copy_all_file(dir_root, dst_root, format='.jpg'):
+    # for su3 case
+    # dir_root = "C:/git/DeepGuider/bin/data/su3"
+    # dst_root = 'C:/git/DeepGuider/bin/data/su3_images'
+
+    dir_list = os.listdir(dir_root)
+    
+    for d in dir_list: # TODO : tqdm
+        file_root = f"{dir_root}/{d}"
+        file_list = os.listdir(file_root)
+        for f in file_list:
+            if format in f:
+                filename = f"{f}{format}"
+                
+                from_filename = f"{file_root}/{f}"
+                to_filename = f"{dst_root}/{d}_{f}"
+    #             print(f"{from_filename}\n{to_filename}\n\n")
+                shutil.copy(from_filename, to_filename)
 
 # # validation dataset 만드는 코드
 # # file list가 있는 txt 읽어서 파일들 옮겨주는 코드
@@ -72,6 +76,9 @@ root - 000 - file.file
 #         shutil.copy(from_filename, to_filename)
 
 def file_copy_txt(txt_filename, root_dir="", dst_dir=""):
+    """
+    tmm17-valid.txt 를 dst_dir 에 옮기는 코드
+    """
     with open(txt_filename, 'r') as f:
         filename_list = f.readlines()
         print(len(filename_list))
@@ -85,12 +92,44 @@ def file_copy_txt(txt_filename, root_dir="", dst_dir=""):
             shutil.copy(from_filename, to_filename)
 
 
+def su3file_copy_sampling(root_path_img, root_path_label, dst_path_img, dst_path_label, sample_N=2000):
+    """
+    su3 데이터 샘플링해서 경로에 저장해줄 예정
+    """
+    file_img_list = os.listdir(root_path_img)
+    sample_list = random.sample(file_img_list, sample_N)
+    sample_list = [s.split('.')[0] for s in sample_list]
+    print(sample_list[:5])
+    for samplename in sample_list:
+        from_filename1 = os.path.join(root_path_img, f"{samplename}.png")
+        to_filename1 = os.path.join(dst_path_img, f"{samplename}.png")
+
+        from_filename2 = os.path.join(root_path_label, f"{samplename}_label.npz")
+        to_filename2 = os.path.join(dst_path_label, f"{samplename}_label.npz")
+        
+        shutil.copy(from_filename1, to_filename1)
+        shutil.copy(from_filename2, to_filename2)
+
+
 
 def main():
-    root_dir ="D:/git/DeepGuider/bin/data/tmm17/vp-labels"
-    dst_dir = "D:/git/DeepGuider/bin/gt_valid"
-    txt_filename= "D:/git/DeepGuider/bin/data/tmm17/valid.txt"
-    file_copy_txt(txt_filename, root_dir, dst_dir)
+    # # valid.txt 안에 있는 파일들 옮겨주는 코드
+    # root_dir ="D:/git/DeepGuider/bin/data/tmm17/vp-labels"
+    # dst_dir = "D:/git/DeepGuider/bin/gt_valid"
+    # txt_filename= "D:/git/DeepGuider/bin/data/tmm17/valid.txt"
+    # file_copy_txt(txt_filename, root_dir, dst_dir)
+
+    # # su3 file copy to dst_root
+    # dir_root = "C:/git/DeepGuider/bin/data/su3"
+    # dst_root = 'J:/git/DeepGuider/bin/data/su3_labels'
+    # copy_all_file(dir_root, dst_root, format='.npz')
+
+    # # su3 - sampling and copying img, label to each directory
+    # root_path_img = "J:/git/DeepGuider/bin/data/su3_images"
+    # root_path_label = "J:/git/DeepGuider/bin/data/su3_labels"
+    # dst_path_img = "J:/git/DeepGuider/bin/data/gt_su3_sample"
+    # dst_path_label = "J:/git/DeepGuider/bin/data/gt_su3_sample_labels"
+    # su3file_copy_sampling(root_path_img, root_path_label, dst_path_img, dst_path_label, sample_N=2000)
 
 
 if __name__=="__main__":
