a452372f1212c92c1a121e31f3361c93be5e69b7
0525stone <0525stone@gmail.com>
2023-05-28 14:28:48 +0900

    검증 완

--- lys_modules/vp_metric_su3_new.py
+++ lys_modules/vp_metric_su3_new.py
@@ -20,7 +20,8 @@ class Su3():
 
         # 필요 파라미터들
         self.gt_format = '.npz' # '.json' TODO : json에 대해서도 읽을 수 있게 해야함ㄴ
-        self.img_ok = False
+        self.gt_img_format = '.png'
+        self.img_ok = False # False True
 
         # points 정보들 들고 있어야하나?
         self.gt_pts = [] # 몇만개의 데이터에 대해서 갖고 있게끔 하는게 맞을까? 그때 그때 읽어서 하는게 나을듯
@@ -38,29 +39,34 @@ class Su3():
 
     def init_data(self):
         self.gt_files = []
+        self.gt_img_files = []
         self.result_files = sorted(os.listdir(self.result_root))
         # gt 파일들은 경로를 불러오는데 방법이 필요함 => 결과 파일명에서 경로를 따로 따내서 해야함
-        for idx, f in enumerate(self.result_files):
+        for f in self.result_files:
             filename = os.path.basename(f)
             f_dir = filename.split('_')[0]
             fname = '_'.join(os.path.basename(f).split('_')[1:]).split('.')[0]
             if self.gt_format=='.npz':
                 gt_filename = f"{self.gt_root}/{f_dir}/{fname}_label{self.gt_format}"
+                gt_img_filename = f"{self.gt_root}/{f_dir}/{fname}{self.gt_img_format}"
             elif self.gt_format=='.json': 
                 gt_filename = f"{self.gt_root}/{f_dir}/{fname}_camera{self.gt_format}"
+                gt_img_filename = f"{self.gt_root}/{f_dir}/{fname}{self.gt_img_format}"
             self.gt_files.append(gt_filename)
+            self.gt_img_files.append(gt_img_filename)
                 
     def process_total_data(self):
         self.degree_list = []
         for idx, (gt, result) in enumerate(zip(self.gt_files, self.result_files)): # FIXME : gt, result없이 그냥 idx만 읽게 해도됨
             assert gt!=result, f"{gt}\t{result}"
-            if idx<10:
+            # if idx<2000:
+            # FIXME : 예외처리해야함. 파일 만약에 없는 경우...
+            gt_pt, result_pt = self.__read_data(idx) # TODO : idx로 img를 불러오려면.. 
+            degree, gt_pt, result_pt = self.process_data(gt_pt, result_pt)
+            self.degree_list.append(degree)
+            if degree<90:
                 if self.img_ok:
-                    self.check_img()
-                # FIXME : 예외처리해야함. 파일 만약에 없는 경우...
-                gt_pt, result_pt = self.__read_data(idx) # TODO : idx로 img를 불러오려면.. 
-                degree, gt_pt, result_pt = self.process_data(gt_pt, result_pt)
-                self.degree_list.append(degree)
+                    self.check_img(idx, gt_pt, result_pt, degree)
         print("================================")
         print(f"result : {len(self.degree_list)}")
         return self.degree_list
@@ -68,7 +74,7 @@ class Su3():
     def process_data(self, gt_pt, result_pt):
         # 이 함수만 따로 쓸 수 있게 인자를 줘서 실행하게끔.. 전역변수로 프로그램 실행하는 것은 안좋dma
         degree, gt = self.__compare_degree(gt_pt, result_pt)
-        print(f"gt : {gt}\tpred : {result_pt}\ndegree : {degree}\n")
+        # print(f"gt : {gt}\tpred : {result_pt}\ndegree : {degree}\n")
         return degree, gt, result_pt
     
     def process_total_result(self):
@@ -78,15 +84,19 @@ class Su3():
     def AA_graph(self, degree_list, AA_upper_th=20):
         aa_list = []
         th_list = []
-        
+        degree_list = sorted(degree_list)
         for th in range(0,AA_upper_th*10,1):
-            th_ = th/1000
-            for idx, e in enumerate(degree_list):
-                if e<th_: 
-                    aa = (len(degree_list)-idx+1)/len(degree_list)
-                    th_list.append(th_*100)
+            th_ = th/10
+            for idx, deg in enumerate(degree_list):
+                if deg>th_: 
+                    # aa = (len(degree_list)-idx)/len(degree_list)
+                    aa = (idx)/len(degree_list)
+                    th_list.append(th_)
                     aa_list.append(aa)
                     break
+                elif idx==len(degree_list)-1:
+                    th_list.append(th_)
+                    aa_list.append(1)
         plt.plot(th_list, aa_list, label=f"result")
 
         print(f"AA@1 eq : {self._AA(th_list, aa_list, 1)}")
@@ -116,10 +126,6 @@ class Su3():
         # print(result_filename)
         gt_pt = self.__read_gt(gt_filename)
         result_pt = self.__read_result(result_filename)
-        
-        if self.img_ok:
-            gt_x, gt_y = self.to_pixel(gt_pt) # FIXME : 입력 gt_pt 조정 필요
-            self.check_img()
         return gt_pt, result_pt
 
     def __read_gt(self,gt_filename):
@@ -140,7 +146,7 @@ class Su3():
         return [pred_x, pred_y]
         
     def __compare_degree(self, gt, result, focal_length=2.1875*256, img_w_h=[512,512]):
-        result = [result[0],result[1],focal_length]
+        result = [result[0]-img_w_h[0]//2,result[1]-img_w_h[1]//2,focal_length]
         final_degree = 180
         final_gt = []
 
@@ -151,8 +157,23 @@ class Su3():
                 final_degree = degree
         return final_degree, final_gt
 
-    def check_img(self):
-        pass
+    def check_img(self, idx, gt_pt, result_pt, degree):
+        gt_x, gt_y = self.to_pixel(gt_pt) # FIXME : 입력 gt_pt 조정 필요
+        img = cv2.imread(self.gt_img_files[idx])
+        img = cv2.circle(img, (int(result_pt[0]), int(result_pt[1])), 5,(0,0,255),3)
+        img = cv2.circle(img, (int(gt_x), int(gt_y)), 5,(0,255,0),3)
+        
+        text1 = f"pred {int(result_pt[0])}, {int(result_pt[1])}"
+        text2 = f"gt {int(gt_x)}, {int(gt_y)}"
+        text3 = f"deg {np.round(degree,2)}"
+        cv2.putText(img, text1, (10,30), cv2.FONT_ITALIC, 1, (255,255,255), 2)
+        cv2.putText(img, text2, (10,70), cv2.FONT_ITALIC, 1, (255,255,255), 2)
+        cv2.putText(img, text3, (10,110), cv2.FONT_ITALIC, 1, (0,0,255), 2)
+        
+
+        cv2.imshow("image",img)
+        cv2.waitKey(-1)
+        cv2.destroyAllWindows()
 
     # # ========================= utils에 뺄까? ==============================
     def gt3points(self, vpts, focal_length=2.1875, h=512, w=512): # 기존 h=480, w=640
@@ -170,7 +191,6 @@ class Su3():
         if gt[2]<0:
             gt[2] = -gt[2]
         gt = [gt[0],-(gt[1]),gt[2]]
-        result = [result[0]-img_w_h[0]//2, result[1]-img_w_h[1]//2, result[2]]
 
         vp_norm = np.linalg.norm(result)
         gt_norm = np.linalg.norm(gt)
@@ -198,8 +218,9 @@ def main():
 
     """
     ground_truth_root = "Y:/git/DeepGuider/bin/data/su3"
-    # result_root = "Y:/git/data_txt/result_su3_sample_new"
-    result_root = "Y:/git/data_txt/result_exp3_su3"
+    result_root = "Y:/git/data_txt/result_su3_sample_new"
+    # result_root = "C:/git/DeepGuider/bin/result_su3_sample_0528"
+    # result_root = "Y:/git/data_txt/result_su3"
 
     su3 = Su3(ground_truth_root,result_root)
     su3.process_total_data()
